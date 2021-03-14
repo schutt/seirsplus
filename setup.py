30a44c3d13ddd7a5090162e0abb7ddef8b748d26
Noel Schutt <1065410+schutt@users.noreply.github.com>
2021-03-14 14:56:22 -0400

    Apply Black to setup file.

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
 import setuptools
 
-# with open("README.md", "r") as fh:
-#     long_description = fh.read()
-
 setuptools.setup(
     packages=setuptools.find_packages(),
     name="seirsplus",
-    version='1.0.9',
-    description='Models of SEIRS epidemic dynamics with extensions, including network-structured populations, testing, contact tracing, and social distancing.',
-    # long_description=long_description,
-    # long_description_content_type="text/markdown",
+    version="1.0.9",
+    description="Models of SEIRS epidemic dynamics with extensions, including network-structured populations, testing, contact tracing, and social distancing.",
     url="https://github.com/ryansmcgee/SEIRS-network-model",
-    author='Ryan Seamus McGee',
-    author_email='ryansmcgee@gmail.com',
-    license='MIT',
-    install_requires=['numpy', 'scipy', 'networkx'],
-    zip_safe=False)
+    author="Ryan Seamus McGee",
+    author_email="ryansmcgee@gmail.com",
+    license="MIT",
+    install_requires=["numpy", "scipy", "networkx"],
+    zip_safe=False,
+)
